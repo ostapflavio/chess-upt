81cb05b56aef982603cec07d7b497a1a669efd30
ostapflavio <ostapflavio45@protonmail.com>
2025-04-12 16:48:49 +0300

    board drawing

--- src/client.c
+++ src/client.c
@@ -1,1 +1,7 @@
 // client side implementation for chess game 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+/// arpa/inet.h required to convert my localhost to a valid IP address 
